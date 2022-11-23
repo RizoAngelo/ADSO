3ebbb334ccae688929af8d181be8d6e6b222d03f
JuanesRa <angelorizo84@gmail.com>
2022-11-23 14:08:01 -0500

    falta por terminar 3 intento

--- actividades en clase/Spotify.py
+++ actividades en clase/Spotify.py
@@ -1,29 +1,40 @@
-lista={}
-while True:
-    Artista=input("nombre del artista: ")
-    if Artista == "":
-        break
-    Cancion=input("nombre de la cancion: ")
-    if Cancion == "":
-        break
-    genero=input("que genero es: ")
-    if genero == "":
-        break
-    Duracion=input("cuanto dura la cancion: ")
-    if Duracion == "":
-        break
-    if Artista:Cancion;genero;Duracion in lista
-    Artista:Cancion;genero;Duracion += (lista,)
-else:
-    Artista:Cancion;genero;Duracion == (lista,)
-    
+Artista={}
+Cancion={}
+genero={}
+Duracion={}
+Spotify={}
 
+def Art(Artita):
+    while True:
+        Artista=input("nombre del artista: ")
+        if Artista == "":
+            break
+        
+def Can(Cancion):
+        while True:
+            Cancion=input("nombre de la cancion: ")
+            if Cancion == "":
+                break
+
+def gen(genero):
+    while True:    
+        genero=input("que genero es: ")
+        if genero == "":
+            break
+
+def tiempo(Duracion):
+    while True:    
+        Duracion=input("cuanto dura la cancion: ")
+        if Duracion == "":
+            break
     anidado={Artista:{
                 "cancion":Cancion,
                 "genero":genero,
                 "duracion":Duracion
                 },}
     print(anidado)
+print(Art("Artita"))
+
 
 
 
