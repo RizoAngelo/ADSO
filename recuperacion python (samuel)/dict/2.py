f37d4c11ae0028debac9b117a7b18e45a44ccc1c
angelo <angelorizo84@gmai.com>
2023-02-03 11:41:38 -0500

    terminado

--- recuperacion python (samuel)/dict/2.py
+++ recuperacion python (samuel)/dict/2.py
@@ -2,6 +2,6 @@ student_id = {1711: "Erick", 1152: "Kyles", 115: "papa"}
 
 print("inicio  Dictionary: ", student_id)
 
-del student_id[111]
+del student_id[1711]
 
 print("final Dictionary ", student_id)
