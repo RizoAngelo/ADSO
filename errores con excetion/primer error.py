9e10d6a3c66f5bd160d6e537ce9ddcec807eceb5
angelo <angelorizo84@gmail.com>
2023-02-24 13:42:28 -0500

    PRUEBA

--- errores con excetion/primer error.py
+++ errores con excetion/primer error.py
@@ -1,13 +1,15 @@
-def numero(dic):
-    a=0
+def numero(num):
     for dic in dicionario:
-        a+=dic[num]
+        if num.v!=dic:
+            return(dic[num])
         raise
         
 try: 
-    num=input("ingrese el numero que quiera buscar en el diccionario: ")
-    numero(num)
-    dicionario=[{"Angelo":1,"Jose":2,"Dario":3,"carlos":4,"Marelis":5}]
+    num=int(input("ingrese el numero que quiera buscar en el diccionario: "))
+    
+    dicionario={"Angelo":1,"Jose":2,"Dario":3,"carlos":4,"Marelis":5}
+    pepe=list(dicionario.index(num))
+    print(numero(pepe))
 
 except KeyError:
     print("no se encuentra la clave que ingreso")
